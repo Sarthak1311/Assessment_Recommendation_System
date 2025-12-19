5d5b72edaf7e01a4e2b2747a28b0b7f27cb89261
sarthak <sarthaktyagi1704@gmail.com>
2025-12-19 08:52:44 +0530

    render test

--- api/app.py
+++ api/app.py
@@ -1,33 +1,37 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import uvicorn
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import uvicorn
 
-# Import recommendation pipeline
-from api.recommendation_engine import recommend
+# # Import recommendation pipeline
+# from api.recommendation_engine import recommend
+
+# app = FastAPI()
+
+# # Health Check Endpoint
+
+# @app.get("/health")
+# def health():
+#     return {"status": "healthy"}
+
+# # Request Model
+
+# class QueryRequest(BaseModel):
+#     query: str
+
+
+# # Recommendation Endpoint
+
+# @app.post("/recommend")
+# def get_recommendations(data: QueryRequest):
+#     query = data.query
+#     response = recommend(query, use_llm=True)   # set True if you want LLM reranker
+#     return response
+
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Health Check Endpoint
-
-@app.get("/health")
-def health():
-    return {"status": "healthy"}
-
-# Request Model
-
-class QueryRequest(BaseModel):
-    query: str
-
-
-# Recommendation Endpoint
-
-@app.post("/recommend")
-def get_recommendations(data: QueryRequest):
-    query = data.query
-    response = recommend(query, use_llm=True)   # set True if you want LLM reranker
-    return response
-
-# Run test server
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/")
+def root():
+    return {"msg": "Render OK"}
